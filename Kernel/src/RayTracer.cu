2d92a1e5ad067dcfa0656b5c4f06cb4e667a2146
zhaolei <zhaolei@naton.cn>
2025-10-27 10:34:39 +0800

    增加抗锯齿（看起来没有特别大的区别）

--- Kernel/src/RayTracer.cu
+++ Kernel/src/RayTracer.cu
@@ -1,13 +1,14 @@
+#include "Hitable.cuh"
+#include "HitableList.cuh"
 #include "Ray.cuh"
 #include "RayTracer.cuh"
+#include "Sphere.cuh"
 #include <cuda.h>
 #include <cuda_gl_interop.h>
 #include <cuda_runtime.h>
+#include <curand_kernel.h>
 #include <device_launch_parameters.h>
 #include <iostream>
-#include "Hitable.cuh"
-#include "Sphere.cuh"
-#include "HitableList.cuh"
 namespace Kernel
 {
 __device__ bool hitSphere(const glm::vec3& center, float radius, const Ray& r)
@@ -20,12 +21,12 @@ __device__ bool hitSphere(const glm::vec3& center, float radius, const Ray& r)
   return (discriminant > 0.0f);
 }
 
-__device__ glm::vec3 color(const Ray& r, Hitable **dWorld)
+__device__ glm::vec3 color(const Ray& r, Hitable** dWorld)
 {
   HitRecord record;
   if ((*dWorld)->hit(r, 0, FLT_MAX, record))
   {
-    return 0.5f*glm::vec3(record.normal.x+1.0f, record.normal.y+1.0f, record.normal.z+1.0f);
+    return 0.5f * glm::vec3(record.normal.x + 1.0f, record.normal.y + 1.0f, record.normal.z + 1.0f);
   }
   else
   {
@@ -50,8 +51,8 @@ __global__ void destroyWorld(Hitable** dList, Hitable** dWorld)
   delete *dWorld;
 }
 
-__global__ void renderInternal(
-  ImageInfo imageInfo, SpaceImageInfo spaceImageInfo, glm::vec3 rayOrigin, Hitable** dWorld)
+__global__ void renderInternal(ImageInfo imageInfo, SpaceImageInfo spaceImageInfo,
+  glm::vec3 rayOrigin, Hitable** dWorld, int nsize, curandState* states)
 {
   int xIndex = blockDim.x * blockIdx.x + threadIdx.x;
   int yIndex = blockDim.y * blockIdx.y + threadIdx.y;
@@ -62,23 +63,44 @@ __global__ void renderInternal(
   }
 
   int pixelIndex = (yIndex * imageInfo.width + xIndex) * 3;
+  int randIndex = yIndex * imageInfo.width + xIndex;
 
-  float x = static_cast<float>(xIndex) / static_cast<float>(imageInfo.width);
-  float y = static_cast<float>(yIndex) / static_cast<float>(imageInfo.height);
+  glm::vec3 c(0, 0, 0);
 
+  for (int i = 0; i < nsize; ++i)
+  {
+    float x = static_cast<float>(xIndex + curand_uniform(&states[randIndex])) /
+      static_cast<float>(imageInfo.width);
+    float y = static_cast<float>(yIndex + curand_uniform(&states[randIndex])) /
+      static_cast<float>(imageInfo.height);
 
+    Ray ray(rayOrigin,
+      spaceImageInfo.mLowerLeftCorner + x * spaceImageInfo.mHorizontal +
+        y * spaceImageInfo.mVertical - rayOrigin);
+    c += color(ray, dWorld);
+  }
 
-  Ray ray(rayOrigin,
-    spaceImageInfo.mLowerLeftCorner + x * spaceImageInfo.mHorizontal +
-      y * spaceImageInfo.mVertical);
-  glm::vec3 c = color(ray, dWorld);
+  c /= static_cast<float>(nsize);
 
   imageInfo.mColor[pixelIndex] = c.x * 255;
   imageInfo.mColor[pixelIndex + 1] = c.y * 255;
   imageInfo.mColor[pixelIndex + 2] = c.z * 255;
 }
 
+__global__ void initRandom(int width, int height, curandState* states)
+{
+  int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+  int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
 
+  int index = yIndex * width + xIndex;
+
+  if (xIndex >= width || yIndex >= height)
+  {
+    return;
+  }
+
+  curand_init(1984, index, 0, &states[index]);
+}
 
 struct RayTracer::Impl
 {
@@ -102,7 +124,8 @@ void RayTracer::unbindImagePBO(GLuint pbo)
 {
   cudaGraphicsUnregisterResource(mImpl->mPBOResource);
 }
-void RayTracer::updateImage(ImageInfo& imageInfo, const SpaceImageInfo& spaceImageInfo, const glm::vec3& rayOrigin)
+void RayTracer::updateImage(
+  ImageInfo& imageInfo, const SpaceImageInfo& spaceImageInfo, const glm::vec3& rayOrigin)
 {
   if (!mImpl->mPBOResource)
   {
@@ -113,27 +136,35 @@ void RayTracer::updateImage(ImageInfo& imageInfo, const SpaceImageInfo& spaceIma
   cudaGraphicsResourceGetMappedPointer(
     reinterpret_cast<void**>(&mImpl->mImageDeviceId), &mImpl->mResourceSize, mImpl->mPBOResource);
 
-  Hitable **dList;
+  Hitable** dList;
   cudaMalloc(&dList, sizeof(Hitable*) * 2);
-  Hitable **dWorld;
+  Hitable** dWorld;
   cudaMalloc(&dWorld, sizeof(Hitable*));
 
-  createWorld<<<1,1>>>(dList, dWorld);
+  createWorld<<<1, 1>>>(dList, dWorld);
+
+  curandState* d_rand_state;
+  cudaMalloc(&d_rand_state, sizeof(curandState) * imageInfo.width * imageInfo.height);
 
   ImageInfo cImageInfo = imageInfo;
   cImageInfo.mColor = mImpl->mImageDeviceId;
   dim3 blockSize(8, 8, 1);
   dim3 gridSize((imageInfo.width + 7) / 8, (imageInfo.height + 7) / 8, 1);
 
-  renderInternal<<<gridSize, blockSize>>>(cImageInfo, spaceImageInfo, rayOrigin, dWorld);
-  auto error = cudaGetLastError();
-  std::cout << __LINE__ << cudaGetErrorName(error) << ": " << cudaGetErrorString(error) << std::endl;
+  initRandom<<<gridSize, blockSize>>>(imageInfo.width, imageInfo.height, d_rand_state);
+
+  // 抗锯齿参数
+  int nSize = 1;
+
+  renderInternal<<<gridSize, blockSize>>>(
+    cImageInfo, spaceImageInfo, rayOrigin, dWorld, nSize, d_rand_state);
+
   cudaDeviceSynchronize();
-  destroyWorld<<<1,1>>>(dList, dWorld);
+
+  destroyWorld<<<1, 1>>>(dList, dWorld);
   cudaFree(dList);
   cudaFree(dWorld);
   cudaGraphicsUnmapResources(1, &mImpl->mPBOResource, nullptr);
-
 }
 
 RayTracer::RayTracer()
